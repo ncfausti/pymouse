6b7c458f6e114059354044f6f84821509f024fc4
Nick Fausti <ncfausti@gmail.com>
2021-12-21 17:42:35 -0600

    Add release event to listener.

--- pymouse.py
+++ pymouse.py
@@ -4,9 +4,11 @@ from pynput.mouse import Listener
 
 def on_click(x, y, button, pressed):
     if pressed:
-        print('{', '"x":{0}, "y":{1}, "button": "{2}"'.format(x, y, button), '}')
+        print('{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, 'Pressed' if pressed else 'Released'), '}')
         sys.stdout.flush()
-
+    else:
+        print('{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, 'Pressed' if pressed else 'Released'), '}')
+        sys.stdout.flush();
 
 with Listener(on_click=on_click) as listener:
     listener.join()
