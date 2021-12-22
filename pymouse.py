4704d37db04d96e116a7130ae58285e1dd283748
Nick Fausti <ncfausti@gmail.com>
2021-12-21 18:30:28 -0600

    Fix typo, [{ instead pf {[

--- pymouse.py
+++ pymouse.py
@@ -5,7 +5,7 @@ from pynput.mouse import Listener
 def on_click(x, y, button, pressed):
     press_or_release = 'Pressed' if pressed else 'Released'
     if pressed:
-        print('{[', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, press_or_release), '},')
+        print('[{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, press_or_release), '},')
         sys.stdout.flush()
     else:
         print('{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, press_or_release), '}]')
