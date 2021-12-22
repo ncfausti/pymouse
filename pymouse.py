55b3d887fcad92a999cddfee0fdbb7844723a5be
Nick Fausti <ncfausti@gmail.com>
2021-12-21 18:23:50 -0600

    Combine into one json response.

--- pymouse.py
+++ pymouse.py
@@ -3,12 +3,13 @@ from pynput.mouse import Listener
 
 
 def on_click(x, y, button, pressed):
+    press_or_release = 'Pressed' if pressed else 'Released'
     if pressed:
-        print('{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, 'Pressed' if pressed else 'Released'), '}')
+        print('{[', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, press_or_release), '},')
         sys.stdout.flush()
     else:
-        print('{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, 'Pressed' if pressed else 'Released'), '}')
-        sys.stdout.flush();
+        print('{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, press_or_release), '}]')
+        sys.stdout.flush()
 
 with Listener(on_click=on_click) as listener:
     listener.join()
