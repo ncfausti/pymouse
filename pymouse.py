f7eba01a3f27479190c2d77a02c10bbc841dac2c
Nick Fausti <ncfausti@users.noreply.github.com>
2021-12-21 19:00:12 -0600

    Send only click released now.

--- pymouse.py
+++ pymouse.py
@@ -4,12 +4,13 @@ from pynput.mouse import Listener
 
 def on_click(x, y, button, pressed):
     press_or_release = 'Pressed' if pressed else 'Released'
-    if pressed:
-        print('[{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, press_or_release), '},')
+    if not pressed:
+        print('{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, press_or_release), '}')
         sys.stdout.flush()
-    else:
-        print('{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, press_or_release), '}]')
-        sys.stdout.flush()
+    # else:
+    #     print('{', '"x":{0}, "y":{1}, "button": "{2}", "action":"{3}"'.format(x, y, button, press_or_release), '}')
+    #     sys.stdout.flush()
+    # sys.stdout.flush()
 
 with Listener(on_click=on_click) as listener:
     listener.join()
